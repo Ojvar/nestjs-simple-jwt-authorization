41a38a71aaeed0bfffd0b39eeb17fab50d9f5477
ojvar <Mojvar.AmirHossein@Gmail.com>
2025-02-16 19:52:58 +0330

    add: prisma org

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ContactModule, AuthModule],
+  imports: [ContactModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
